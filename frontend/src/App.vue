93719859c94c7c6e9d92b294aadaa5e5c28c01e2
mark doerr <mark.doerr@uni-greifswald.de>
2023-06-27 18:08:53 +0200

    fix: grpc-generator-image updated; envoy Dockerfile updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,18 @@
-<template>
-  <div id="app">
-    <Time />
-  </div>
-</template>
-
-<script>
+<script setup>
 import Time from './components/Time.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Time
-  }
-}
 </script>
 
-<style>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <Time />
+  </main>
+</template>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +20,32 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
